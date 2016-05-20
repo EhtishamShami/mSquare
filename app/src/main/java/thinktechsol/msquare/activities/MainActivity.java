d53bf169d38463181e4028c49d29468c6784aa08
ArshadIqbal <k.arshad063@gmail.com>
2016-05-20 14:04:28 +0500

    Commiting Msquare whole project to git

---
+++ app/src/main/java/thinktechsol/msquare/activities/MainActivity.java
@@ -0,0 +1,36 @@
+package thinktechsol.msquare.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import thinktechsol.msquare.R;
+import thinktechsol.msquare.adapter.ItemAdapter;
+import thinktechsol.msquare.model.Item;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView simpleCustomeListView = (ListView) findViewById(R.id.listView);
+
+        Item weather_data[] = new Item[]
+                {
+                        new Item(R.drawable.access_icon, "Access File"),
+                        new Item(R.drawable.doc_icon, "Word File"),
+                        new Item(R.drawable.excel_icon, "Excel File"),
+                        new Item(R.drawable.txt_icon, "Text File"),
+                        new Item(R.drawable.access_icon, "Access File"),
+                        new Item(R.drawable.doc_icon, "Word File"),
+                        new Item(R.drawable.excel_icon, "Excel File"),
+                        new Item(R.drawable.txt_icon, "Text File")
+                };
+        ItemAdapter myAdapter = new ItemAdapter(this,
+                R.layout.listview_item_row, weather_data);
+        simpleCustomeListView.setAdapter(myAdapter);
+    }
+}
